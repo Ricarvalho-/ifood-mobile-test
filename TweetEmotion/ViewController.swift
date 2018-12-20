0c9713adf5e04bedcfb4276520179dd25ac0f1aa
Ricardo Carvalho <ricardo.tks.best@gmail.com>
2018-12-19 22:19:01 -0200

    Project initial files

---
+++ TweetEmotion/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TweetEmotion
+//
+//  Created by Ricardo Carvalho on 17/12/18.
+//  Copyright © 2018 Ricardo Carvalho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
