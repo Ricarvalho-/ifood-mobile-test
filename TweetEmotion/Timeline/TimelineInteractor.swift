486674ed232a7e605fe76b93b8402dd92b76b0ad
Ricardo Carvalho <ricardo.tks.best@gmail.com>
2018-12-21 01:08:15 -0200

    Timeline interactor implementation

--- TweetEmotion/Timeline/TimelineInteractor.swift
+++ TweetEmotion/Timeline/TimelineInteractor.swift
@@ -12,9 +12,76 @@ protocol TimelineInteractorDelegate: class {
     func didRetrieveTweets(_ tweets: [Tweet])
 }
 
+protocol TimelineWorker {
+    func fetchTweets(after last: Tweet?, for user: User, _ completion: ([Tweet]?) -> Void)
+    func cancelFetch()
+}
+
 protocol TimelineInteractor {
     func retrieveInitialTweets(for user: User)
     func retrieveMoreTweets()
     func cancelPendingTasks()
-    init(with delegate: TimelineInteractorDelegate?) // weak
+    init(with delegate: TimelineInteractorDelegate?)
+}
+
+class TimelineInteractorImpl: TimelineInteractor {
+    weak var delegate: TimelineInteractorDelegate?
+    private var tweets = [Tweet]()
+    private var user: User?
+    private var scheduleMoreWorkAfterCompletion = false
+    
+    private lazy var workerChainManager = ChainManager<TimelineWorker, (User, [Tweet])>(
+        with: [TimelineWorker](),
+        onEachStart: { [weak self] worker, params in
+            self?.start(worker, with: params)
+        },
+        onStopCurrent: { $0.cancelFetch() }
+    )
+    
+    required init(with delegate: TimelineInteractorDelegate?) {
+        self.delegate = delegate
+    }
+    
+    func retrieveInitialTweets(for user: User) {
+        self.user = user
+        tweets.removeAll()
+        cancelPendingTasks()
+        retrieveMoreTweets()
+    }
+    
+    func retrieveMoreTweets() {
+        guard let user = user else { return }
+        guard !workerChainManager.isRunning else {
+            scheduleMoreWorkAfterCompletion = true
+            return
+        }
+        let tweets = self.tweets
+        DispatchQueue.global(qos: .userInitiated).async { [weak self] in
+            self?.workerChainManager.begin(with: (user, tweets))
+        }
+    }
+    
+    private func start(_ worker: TimelineWorker, with params: (user: User, tweets: [Tweet])) {
+        worker.fetchTweets(after: params.tweets.first, for: params.user) { [weak self] results in
+            guard let results = results else {
+                self?.workerChainManager.startNext(with: params)
+                return
+            }
+            self?.workerChainManager.stop()
+            
+            self?.tweets.append(contentsOf: results)
+            
+            DispatchQueue.main.async {
+                self?.delegate?.didRetrieveTweets(results)
+            }
+            
+            if (self?.scheduleMoreWorkAfterCompletion ?? false) {
+                self?.retrieveMoreTweets()
+            }
+        }
+    }
+    
+    func cancelPendingTasks() {
+        workerChainManager.stop()
+    }
 }
